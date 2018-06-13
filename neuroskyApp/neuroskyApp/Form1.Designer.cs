0d553f2495964c4d3f2c6e62da6a204031e3c0cf
yalin1997 <godjj1997@gmail.com>
2018-06-13 16:59:53 +0800

    刪掉上一頁按鈕

--- neuroskyApp/neuroskyApp/Form1.Designer.cs
+++ neuroskyApp/neuroskyApp/Form1.Designer.cs
@@ -32,17 +32,16 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("新細明體", 12F);
-            this.button1.Location = new System.Drawing.Point(112, 11);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(84, 9);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(163, 35);
+            this.button1.Size = new System.Drawing.Size(122, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Disconnect";
             this.button1.UseVisualStyleBackColor = true;
@@ -50,10 +49,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(281, 11);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(211, 9);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(170, 35);
+            this.button2.Size = new System.Drawing.Size(128, 28);
             this.button2.TabIndex = 1;
             this.button2.Text = "查看資料";
             this.button2.UseVisualStyleBackColor = true;
@@ -63,38 +62,28 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 12F);
-            this.label1.Location = new System.Drawing.Point(95, 527);
+            this.label1.Location = new System.Drawing.Point(71, 422);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.Size = new System.Drawing.Size(76, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "系統訊息:";
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(98, 65);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Location = new System.Drawing.Point(64, 50);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(840, 442);
+            this.panel1.Size = new System.Drawing.Size(630, 354);
             this.panel1.TabIndex = 3;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button3.Location = new System.Drawing.Point(15, 13);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(60, 33);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "←";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(457, 13);
+            this.button4.Location = new System.Drawing.Point(343, 9);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(149, 33);
+            this.button4.Size = new System.Drawing.Size(112, 28);
             this.button4.TabIndex = 5;
             this.button4.Text = "更改受測者資料";
             this.button4.UseVisualStyleBackColor = true;
@@ -102,16 +91,15 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1034, 596);
+            this.ClientSize = new System.Drawing.Size(776, 477);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "neuroskyApp";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -126,7 +114,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
     }
 }
