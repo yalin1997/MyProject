8967dc2c5257c1a8c45ea71d5a64180b7091650a
yalin1997 <godjj1997@gmail.com>
2018-07-18 21:41:09 +0800

    繪圖繪製rawdata

--- neuroskyApp/neuroskyApp/Form1.Designer.cs
+++ neuroskyApp/neuroskyApp/Form1.Designer.cs
@@ -38,21 +38,21 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("新細明體", 12F);
-            this.button1.Location = new System.Drawing.Point(84, 9);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(31, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 28);
+            this.button1.Size = new System.Drawing.Size(163, 35);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Disconnect";
+            this.button1.Text = "Connect";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(211, 9);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(212, 11);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(128, 28);
+            this.button2.Size = new System.Drawing.Size(171, 35);
             this.button2.TabIndex = 1;
             this.button2.Text = "查看資料";
             this.button2.UseVisualStyleBackColor = true;
@@ -62,28 +62,29 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 12F);
-            this.label1.Location = new System.Drawing.Point(71, 422);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(61, 529);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.Size = new System.Drawing.Size(94, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "系統訊息:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(64, 50);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Location = new System.Drawing.Point(31, 50);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(630, 354);
+            this.panel1.Size = new System.Drawing.Size(788, 444);
             this.panel1.TabIndex = 3;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(343, 9);
-            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Location = new System.Drawing.Point(399, 11);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(112, 28);
+            this.button4.Size = new System.Drawing.Size(149, 35);
             this.button4.TabIndex = 5;
             this.button4.Text = "更改受測者資料";
             this.button4.UseVisualStyleBackColor = true;
@@ -91,15 +92,15 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(776, 477);
+            this.ClientSize = new System.Drawing.Size(1035, 596);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "neuroskyApp";
             this.Load += new System.EventHandler(this.Form1_Load);
--- neuroskyApp/neuroskyApp/Form1.cs
+++ neuroskyApp/neuroskyApp/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using libStreamSDK;
 using System.Globalization;
+using System.Threading;
 
 namespace neuroskyApp
 {
@@ -17,6 +18,14 @@ namespace neuroskyApp
         private int errCode;
         private int ConnectionID ;
         public int callNum = 0;
+        private List<float> rawList = new List<float>();
+        private Boolean ifConnect = false;
+        private Boolean startDraw = false;
+        private Pen bluePen = new Pen(Color.Blue);
+        private Point p1 ;
+        private Point p2 ;
+        private int iCount = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -27,25 +36,6 @@ namespace neuroskyApp
         private void Form1_Load(object sender, EventArgs e)
         {
             
-            // if (callNum == 1)
-            {
-                String comPortName = "\\\\.\\COM3";
-                ConnectionID = NativeThinkgear.TG_GetNewConnectionId();
-                errCode = NativeThinkgear.TG_Connect(ConnectionID,
-                              comPortName,
-                              NativeThinkgear.Baudrate.TG_BAUD_57600,
-                              NativeThinkgear.SerialDataFormat.TG_STREAM_PACKETS);
-                if (errCode < 0)
-                {
-                    Console.WriteLine("ERROR: TG_Connect() returned: " + errCode);
-                    label1.Text = ("ERROR: TG_Connect() returned: " + errCode);
-                }
-                else
-                {
-                    label1.Text = ("Connect success!!!");
-                    AutoGetValue();
-                }
-            }
 
         }
         private void Form1_Activated(object sender, System.EventArgs e) {
@@ -70,8 +60,10 @@ namespace neuroskyApp
                     if (MessageBox.Show("您將開始與裝置的連線，確定嗎?", "My Application",
                     MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk) == DialogResult.OK)
                     {
-                        String comPortName = "\\\\.\\COM3";
+                        String comPortName = "\\\\.\\COM4";
                     ConnectionID = NativeThinkgear.TG_GetNewConnectionId();
+                    NativeThinkgear.TG_SetStreamLog(ConnectionID, "myStreamLog.txt");
+                    NativeThinkgear.TG_SetDataLog(ConnectionID, "myDataLog.txt");
                     errCode = NativeThinkgear.TG_Connect(ConnectionID,
                                       comPortName,
                                       NativeThinkgear.Baudrate.TG_BAUD_57600,
@@ -80,39 +72,94 @@ namespace neuroskyApp
                         {
                             Console.WriteLine("ERROR: TG_Connect() returned: " + errCode);
                             label1.Text = ("ERROR: TG_Connect() returned: " + errCode);
+                        ifConnect = false;
+                        startDraw = false;
                         }
                         else
                         {
                             label1.Text = ("Connect success!!!");
                             button1.Text = ("Disconnect");
-                            AutoGetValue();
-                        }
+                            ifConnect = true;
+                        //AutoGetValue();
+
+                    }
                      }
               }
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
+            int x1 = 20;
+            int x2 = 320;
+            Graphics g1 = panel1.CreateGraphics();
 
+            if(startDraw == true)
+            {
+                for(int j = 0; j < rawList.Count() - 249; j++)
+                {
+                    g1.DrawLine(bluePen, 50, x1, 50, x2);
+                    g1.DrawLine(bluePen, 50, x2 / 2, 500, x2 / 2);
+                    for (int i = 0; i < 249; i++)
+                    {
+                        Console.WriteLine(rawList[i]);
+                        int addressY = (x2 / 2) - (((x2 / 2) - x1) * (int)rawList[j + i] / 1000);
+                        int addressX = i * 2 + 50;
+                        int nextY = (x2 / 2) - (((x2 / 2) - x1) * (int)rawList[j + i + 1] / 1000);
+                        int nextX = (i + 1) * 2 + 50;
+                        g1.DrawLine(bluePen, addressX, addressY, nextX, nextY);
+                    }
+                    if(j==rawList.Count() - 250)
+                    {
+
+                    }
+                    else
+                    {
+                        Thread.Sleep(500);
+                        g1.Clear(Color.White);
+                    }
+                }
+               
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (errCode < 0)
+            startDraw = true;
+            if (ifConnect == true)
             {
-                Console.WriteLine("not connect." + errCode);
-                label1.Text = ("haven't connect." + errCode);
+                int packetsRead = 0;
+                while (packetsRead < 2000)
+                {
+
+                    /* Attempt to read a Packet of data from the connection */
+                    errCode = NativeThinkgear.TG_ReadPackets(ConnectionID, 1);
+                    Console.WriteLine("TG_ReadPackets returned: " + errCode);
+                    /* If TG_ReadPackets() was able to read a complete Packet of data... */
+                    if (errCode == 1)
+                    {
+                        packetsRead++;
+
+                        /* If attention value has been updated by TG_ReadPackets()... */
+                        if (NativeThinkgear.TG_GetValueStatus(ConnectionID, NativeThinkgear.DataType.TG_DATA_RAW) != 0)
+                        {
+                            float temp = NativeThinkgear.TG_GetValue(ConnectionID, NativeThinkgear.DataType.TG_DATA_RAW);
+                            rawList.Add(temp);
+                            /* Get and print out the updated attention value */
+                            Console.WriteLine("New RAW value: : " + (int)temp);
+                            label1.Text = "New RAW value: : " + (int)temp;
+                        } /* end "If attention value has been updated..." */
+
+                    } /* end "If a Packet of data was read..." */
+
+                } /* end "Read 10 Packets of data from connection..." */
+                panel1.Invalidate();
+                //AutoGetValue();
             }
-            else {
-                if (NativeThinkgear.TG_GetValueStatus(ConnectionID, NativeThinkgear.DataType.TG_DATA_RAW) != 0) {
-                    TimeSpan CurrTime = DateTime.Now.TimeOfDay;
-                    String CurrTimeSTR = DateTime.Now.ToString("h:mm:ss tt");
-                  Console.WriteLine( "%s:  raw:   %d\n", CurrTimeSTR, (int)NativeThinkgear.TG_GetValue(ConnectionID,
-                        NativeThinkgear.DataType.TG_DATA_RAW) );
-                   label1.Text = ("%s:  raw:   %d\n"+ CurrTimeSTR + NativeThinkgear.TG_GetValue(ConnectionID,
-                        NativeThinkgear.DataType.TG_DATA_RAW));
-                }
+            else
+            {
+                label1.Text = "you have to connect first!";
             }
+
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -134,19 +181,48 @@ namespace neuroskyApp
         }
         private void AutoGetValue()
         {
-            int autoGet = NativeThinkgear.TG_EnableAutoRead(ConnectionID, -1);
-            
-            Task.Run(() => {
-                while (autoGet == 0)
+            Console.Write("讀取開始");
+            int autoGet = NativeThinkgear.TG_EnableAutoRead(ConnectionID, 1);
+            NativeThinkgear.MWM15_setFilterType(ConnectionID, NativeThinkgear.FilterType.MWM15_FILTER_TYPE_60HZ);
+            int packetsRead = 0;
+
+            Task t = Task.Run(() => {
+                while (packetsRead < 2000) // it use as time
                 {
-                    if(NativeThinkgear.TG_GetValueStatus(ConnectionID, NativeThinkgear.DataType.TG_DATA_RAW) != 0)
+                    /* If raw value has been updated ... */
+                    if (NativeThinkgear.TG_GetValueStatus(ConnectionID, NativeThinkgear.DataType.TG_DATA_RAW) != 0)
                     {
-                      DateTime time = DateTime.Now;
-                      float tempData=NativeThinkgear.TG_GetValue(ConnectionID, NativeThinkgear.DataType.TG_DATA_RAW);
-                        label1.Text = "收到數據 : " + tempData;
+                        if (NativeThinkgear.TG_GetValueStatus(ConnectionID, NativeThinkgear.DataType.MWM15_DATA_FILTER_TYPE) != 0)
+                        {
+                            Console.WriteLine(" Find Filter Type:  " + NativeThinkgear.TG_GetValue(ConnectionID, NativeThinkgear.DataType.MWM15_DATA_FILTER_TYPE) + " index: " + packetsRead);
+                            break;
+                        }
+                        float TEMP = NativeThinkgear.TG_GetValue(ConnectionID, NativeThinkgear.DataType.TG_DATA_RAW);
+                        /* Get and print out the updated raw value */
+                        rawList.Add(TEMP);
+                        PrintRaw(TEMP);
+                        packetsRead++;
+                        panel1.Invalidate();
+                        if (packetsRead == 800 || packetsRead == 1600)  // call twice interval than 1s (512)
+                        {
+                            errCode = NativeThinkgear.MWM15_getFilterType(ConnectionID);
+                            Console.WriteLine(" MWM15_getFilterType called: " + errCode);
+                        }
                     }
                 }
+
             });
+            t.Wait();
+        }
+
+        private static void PrintRaw(float TEMP)
+        {
+            Console.WriteLine("new RAW Value"+(int)TEMP);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
